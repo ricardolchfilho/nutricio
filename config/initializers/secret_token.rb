1880281592200eda5b87a60b660f4f395fa66b72
Lucas Medeiros <lucastoc@gmail.com>
2011-03-18 14:50:25 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nutricio::Application.config.secret_token = '645f19b5e17e45e09d6c200c875b745c2abad48224987c371aa87738977f95a3ffcbb8a06836d2d44eefa10a6c09d37483c8891044f83371e966e6ac8a39ad18'
